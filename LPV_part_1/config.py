3a7a1373e60e41c2280cd8608d7cf656a03b8f5d
Steven Lahoud <steven.lahoud.etu@univ-lille.fr>
2025-05-20 16:02:23 +0200

    Documentation of the function

--- LPV_part_1/config.py
+++ LPV_part_1/config.py
@@ -1,6 +1,25 @@
 import numpy as np
 
 def initialize_parameters() :
+    """
+    Initializes and returns system parameters for an LPV model
+    
+    Returns:
+        dict: A dictionary containing the following keys:
+            - A (np.ndarray): State transition matrices. 
+            - K (np.ndarray): Disturbance gain matrices. 
+            - C (np.ndarray): Output matrix.
+            - F (np.ndarray): Noise matrix. 
+            - D (np.ndarray): Input-output gain matrix.
+            - nx (int): Number of states.
+            - ny (int): Number of outputs.
+            - nu (int): Number of inputs.
+            - np (int): Number of scheduling parameters.
+            - N (int): Number of training samples.
+            - Nval (int): Number of validation samples.
+            - Ntot (int): Total number of samples (N + Nval).
+    """
+    
     A = np.zeros((2,2,2))
     A[:, :, 0] = np.array([[0.4, 0.4], [0.2, 0.1]])
     A[:, :, 1] = np.array([[0.1, 0.1], [0.2, 0.3]])
