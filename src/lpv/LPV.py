04acc4fbb99facde41ef18d594ade4e96d1f17f1
Steven Lahoud <steven.lahoud.etu@univ-lille.fr>
2025-05-23 15:07:07 +0200

    Implementation of the function isEquivalence in LPV using Markov Parameters on 3D matrix, the results are the same as the one in matlab

--- src/lpv/LPV.py
+++ src/lpv/LPV.py
@@ -98,7 +98,65 @@ class LPV:
     def simulate_Innovation(self):
         return None
     
-    def isEquivalent(self):
-        return None
-    
+    def isEquivalent(self, other, x0, x0_other, tolerance=None):
+        """
+        Check if this LPV is equivalent to another LPV system by comparing their
+        Markov parameters up to a certain order.
+        
+        Parameters:
+            other : LPV
+                    Another LPV instance to compare against.
+            x0    : ndarray
+                    Initial state vector of this LPV system
+            x0_other : ndarray
+                       Initial state vector of the other LPV system.
+            tolerance : float, optional
+                        Numerical tolerance for equivalence check, default is 1e-5.
+                        
+        Returns:
+            bool: True if the two LPV systems are equivalent within the tolerance, False otherwise.
+        """
+        if tolerance is None:
+            tolerance = 1e-5
+            
+        n1, n2 = self.nx, other.nx
+        D1, D2 = self.np, other.np
+        
+        if D1 != D2:
+            raise ValueError("The two systems should have the same number of dimensions")
+        
+        D= D1
+        ny = self.ny
+        N = 2 * max(n1, n2) - 1
+
+        x0_col = x0.reshape(-1, 1)
+        x0_other_col = x0_other.reshape(-1, 1)
+
+        R = np.vstack([
+            np.hstack([x0_col] + [self.B[:, :, i] for i in range(D)]),
+            np.hstack([x0_other_col] + [other.B[:, :, i] for i in range(D)])
+        ])
+
+        compC = np.hstack([self.C, -other.C])
+
+        for step in range(N + 1):
+            diff_markov = compC @ R # C1*(B1 or A1^k B1)-C2*(B2 or A2^k B2)
+            if np.linalg.norm(diff_markov, 2) > tolerance:
+                print("Not equivalent - Difference in Markov parameters: ")
+                print(diff_markov)
+                return False
+            
+            R_new = []
+            for i in range(D):
+                A1_block = self.A[:, :, i]
+                A2_block = other.A[:, :, i]
+                block = np.block([
+                    [A1_block, np.zeros((n1, n2))],
+                    [np.zeros((n2, n1)), A2_block]
+                ])
+                R_new.append(block @ R)
+            R = np.hstack(R_new)
+        return True
+
+
         
