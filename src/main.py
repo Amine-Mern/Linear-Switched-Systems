9786a51c9373c52502860a1657bf4bc8bd4a85ee
Amine <mernissiamine@hotmail.com>
2025-05-26 13:22:52 +0200

    Avancement du Test de SimulateInnovation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from config import initialize_parameters
+from lpv.LPV import LPV
 
 params = initialize_parameters()
 
@@ -8,3 +9,7 @@ Ntot = params['Ntot']
 ny, np_, nx, nu = params['ny'], params['np'], params['nx'], params['nu']
 A, K, C, F, D = params['A'], params['K'], params['C'], params['F'], params['D']
 N, Nval, Ntot = params['N'], params['Nval'], params['Ntot']
+
+print("Test : Simulate_Innovation")
+LpvSys = LPV(A,np.zeros,C,K=K,F=F)
+LpvSys.simulate_Innovation(Ntot,y,p)
